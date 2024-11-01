1be115bc6d12cd4f61debaf823c3b604253e09e6
misha-fedyna <mihajlofedina5@gmail.com>
2024-11-01 21:23:20 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab9.2.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestFormula1)
+		{
+			// Тест для calculateWFormula1
+			double x = 5.0, y = 2.0, z = 4.0;
+			double expected = MIN(SQUARE(x - z), x - y);  // Розрахунок очікуваного значення вручну
+			Assert::AreEqual(expected, calculateWFormula1(x, y, z), 0.001);
+		}
+
+		TEST_METHOD(TestFormula2)
+		{
+			// Тест для calculateWFormula2
+			double x = 3.0, y = 4.0, z = 2.0;
+			double expected = MAX(x * y, y * z) + MIN(z * x, y * z);  // Розрахунок очікуваного значення вручну
+			Assert::AreEqual(expected, calculateWFormula2(x, y, z), 0.001);
+		}
+
+		TEST_METHOD(TestCalculateW_Formula1)
+		{
+			// Тест для calculateW з використанням Formula1 (z > 3)
+			double x = 5.0, y = 2.0, z = 4.0;
+#define USE_FORMULA1
+			double expected = calculateWFormula1(x, y, z);
+			Assert::AreEqual(18, calculateW(x, y, z), 0.001);
+#undef USE_FORMULA1
+		}
+
+		TEST_METHOD(TestCalculateW_Formula2)
+		{
+			// Тест для calculateW з використанням Formula2 (z <= 3)
+			double x = 3.0, y = 4.0, z = 3.0;
+			double expected = calculateWFormula2(x, y, z);
+			Assert::AreEqual(expected, calculateW(x, y, z), 0.001);
+		}
+	};
+}
---
+++ lab9.2.1.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+// Макроси для основних операцій
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define ABS(a) ((a) < 0 ? -(a) : (a))
+#define POWER(a, b) (pow((a), (b)))
+#define SQUARE(a) ((a) * (a))
+
+// Функції для обчислення w з різними формулами
+double calculateWFormula1(double x, double y, double z) {
+    return MIN(SQUARE(x - z), x - y);
+}
+
+double calculateWFormula2(double x, double y, double z) {
+    return MAX(x * y, y * z) + MIN(z * x, y * z);
+}
+
+// Функція для вибору формули на основі значення z
+double calculateW(double x, double y, double z) {
+#ifdef USE_FORMULA1
+    return calculateWFormula1(x, y, z);
+#else
+    return calculateWFormula2(x, y, z);
+#endif
+}
+
+int main() {
+    double x, y, z;
+
+    // Введення значень змінних від користувача
+    cout << "Enter values for x, y, and z: ";
+    cin >> x >> y >> z;
+
+    // Використання умови для вибору потрібної формули
+    if (z > 3) {
+#define USE_FORMULA1
+    }
+    else {
+#undef USE_FORMULA1
+    }
+
+    // Обчислення w
+    double w = calculateW(x, y, z);
+
+    // Виведення результату
+    cout << "The result of w is: " << w << endl;
+
+    return 0;
+}
